f33a1c42c7962861317473a7e60e2614e1a4c23d
junming su <junmingsu@junmingdeMacBook-Pro.local>
2017-12-25 15:27:47 +0800

    feat: add react-16-admin initialization

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import logo from './logo.svg';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+      </div>
+    );
+  }
+}
