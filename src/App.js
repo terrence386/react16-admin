84d451d51772725f6ac26ffc3e5978bc0a29cb83
junming su <junmingsu@junmingdeMacBook-Pro.local>
2017-12-26 18:21:34 +0800

    feat: add commonFun

--- src/App.js
+++ src/App.js
@@ -4,18 +4,31 @@ import {
   Route,
   Link
 } from 'react-router-dom'
+import {helloSaga} from './saga'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+import {trim} from './javascript/commonFun';
 import logo from './logo.svg';
 import './App.css';
-
+const sagaMiddleware = createSagaMiddleware(helloSaga)
+const store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+)
 export default class App extends Component {
   render() {
+    console.log('公共函数一:去除空格(另外几十种方法commonFun查看)');
+    let a=" a   "
+    console.log(trim(a,1));
     return (
+      <store>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
       </div>
+      </store>
     );
   }
 }
